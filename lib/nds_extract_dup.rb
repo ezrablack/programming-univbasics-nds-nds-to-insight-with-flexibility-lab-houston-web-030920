3f6312141184727e0f623af7389e633a6fb9bf29
Timothy Morales <timothydanielmorales@gmail.com>
2020-03-09 21:09:38 +0000

    Automatically backed up by Learn

--- lib/nds_extract_dup.rb
+++ lib/nds_extract_dup.rb
@@ -1,4 +1,4 @@
- # Provided, don't edit
+# Provided, don't edit
 require 'directors_database'
 
 # A method we're giving you. This "flattens"  Arrays of Arrays so: [[1,2],
@@ -34,21 +34,22 @@ end
 # Your code after this point
 
 def movies_with_director_key(name, movies_collection)
-  movies_collection.each do |m, n|
-    puts n 
-  end
-end
   # GOAL: For each Hash in an Array (movies_collection), provide a collection
   # of movies and a directors name to the movie_with_director_name method
   # and accumulate the returned Array of movies into a new Array that's
   # returned by this method.
+  #
   # INPUT:
   # * name: A director's name
   # * movies_collection: An Array of Hashes where each Hash represents a movie
+  #
   # RETURN:
+  #
   # Array of Hashes where each Hash represents a movie; however, they should all have a
   # :director_name key. This addition can be done by using the provided
   # movie_with_director_name method
+end
+
 
 def gross_per_studio(collection)
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
